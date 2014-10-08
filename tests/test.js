e74369d4ba5631406559c6447cb3f800dca46b36
Steve Zhu <st.zhu1@gmail.com>
2014-10-08 16:52:22 -0400

    Removed 1 test

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,3 @@
 Tinytest.add("lodash - Exists", function(test) {
-	test.equal(_, lodash, "Expected _ to be lodash");
 	test.notEqual(typeof lodash, 'undefined', "Expected lodash to not be undefined");
 });
