4a2e97fbd5c9bf89569bc0cc8ae31d80a84dcb49
Steve Zhu <st.zhu1@gmail.com>
2014-10-08 15:43:56 -0400

    Should be notEqual

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
 Tinytest.add("lodash - Exists", function(test) {
-	test.equal(typeof lodash, 'undefined', "Expected lodash to be defined");
+	test.notEqual(typeof lodash, 'undefined', "Expected lodash to be defined");
 	test.equal(_, lodash, "Expected _ to be lodash");
 });
