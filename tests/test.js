21e2344f410294de23f7763dd0ef40260f71c3af
Steve Zhu <st.zhu1@gmail.com>
2015-03-11 11:21:43 -0400

    Add test to check that _ and lodash are equal

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,7 @@
 Tinytest.add("lodash - Exists", function(test) {
 	test.notEqual(typeof lodash, 'undefined', "Expected lodash to not be undefined");
 });
+
+Tinytest.add("lodash - lodash equals _", function(test) {
+	test.equal(lodash, _, "Expected lodash to be equal to _");
+});
