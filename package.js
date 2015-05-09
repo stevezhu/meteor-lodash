e5817a291d971124c43944ccd1e9cf18835df519
Steve Zhu <st.zhu1@gmail.com>
2015-05-09 10:29:30 -0400

    Lodash v3.8.0

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
-Npm.depends({'lodash': '3.7.0'});
+Npm.depends({'lodash': '3.8.0'});
 
 Package.describe({
 	name: "stevezhu:lodash",
 	summary: "A utility library delivering consistency, customization, performance, & extras.",
-	version: "3.7.0",
+	version: "3.8.0",
 	git: "https://github.com/stevezhu/meteor-lodash.git"
 });
 
