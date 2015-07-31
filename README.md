217071a4a3444e47fd07e94455d5020f06499831
Steve Zhu <st.zhu1@gmail.com>
2015-07-31 00:10:21 -0400

    Lodash v3.10.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A meteor package for [lodash](http://lodash.com/).
 
-Using lodash v3.9.3
+Using lodash v3.10.0
 
 
 ## Usage
--- package.js
+++ package.js
@@ -1,9 +1,9 @@
-Npm.depends({'lodash': '3.9.3'});
+Npm.depends({'lodash': '3.10.0'});
 
 Package.describe({
 	name: "stevezhu:lodash",
 	summary: "A utility library delivering consistency, customization, performance, & extras.",
-	version: "3.9.3",
+	version: "3.10.0",
 	git: "https://github.com/stevezhu/meteor-lodash.git"
 });
 
